85e396a27da067cf8360d49bdd2d5037e6987207
fhilips <fhilips.souza@gmail.com>
2021-09-07 22:15:52 -0300

    movie list pagination finished

--- frontend/src/pages/MovieList/index.tsx
+++ frontend/src/pages/MovieList/index.tsx
@@ -1,55 +1,65 @@
 import { AxiosRequestConfig } from 'axios';
-import { useEffect, useState } from 'react';
+import Pagination from 'components/Pagination';
+import { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Movie } from 'types/Movies';
+import { SpringPage } from 'types/vendor/spring';
 import { requestBackend } from 'utils/request';
 import MovieCard from './MovieCard';
 
 import './styles.scss';
 
-type movieList = {
+type movieListPage = {
   content: Movie[];
+  totalPages: number;
 };
 
 const MovieList = () => {
-  const [moviesList, setMoviesList] = useState<movieList>();
+  // const [moviesList, setMoviesList] = useState<SpringPage<movieListPage>>();
+  const [moviesList, setMoviesList] = useState<movieListPage>();
 
-  useEffect(() => {
+  const getMovies = useCallback((pageNumber: number) => {
     const params: AxiosRequestConfig = {
       url: '/movies',
       withCredentials: true,
       params: {
-        page: 0,
-        size: 12,
+        page: pageNumber,
+        size: 4,
       },
     };
     requestBackend(params).then((response) => {
       setMoviesList(response.data);
       console.log(response.data);
     });
-  }, []);
+  }, [])
+
+  useEffect(() => {
+    getMovies(0)
+  }, [getMovies]);
 
   return (
     <main className="movie-list-container">
       <h1 className="movie-list-title">Tela listagem de filmes</h1>
       <div className="row">
-      {moviesList?.content.map((movie) => {
-        return (
-          <Link
-            className="col-sm-6 col-md-6 col-xl-3"
-            to={`/movies/${movie.id}`}
-            key={movie.id}            
-          >
-            <MovieCard movie={movie} key={movie.id} />
-          </Link>
-        );
-      })}
-
+        {moviesList?.content.map((movie) => {
+          return (
+            <Link
+              className="col-sm-6 col-md-6 col-xl-3"
+              to={`/movies/${movie}`}
+              key={movie.id}
+            >
+              <MovieCard movie={movie} key={movie.id} />
+            </Link>
+          );
+        })}
+        <Pagination 
+          pageCount={(moviesList) ? moviesList?.totalPages : 0} 
+          range={3}
+          onChange={getMovies}
+          />
       </div>
-      
     </main>
   );
 };
 
 export default MovieList;
-
